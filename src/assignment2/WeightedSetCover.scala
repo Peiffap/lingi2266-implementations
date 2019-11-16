6646c7a42b16b3798312e2b89845e1c8cae21bc6
Gilles Peiffer <gilles.peiffer@outlook.com>
2019-11-16 04:22:27 +0100

    Update name assignment 2, make variable names inginious-compatible

--- src/assignment2/WeightedSetCover.scala
+++ src/assignment2/WeightedSetCover.scala
@@ -3,7 +3,7 @@ package assignment2
 import scala.io.Source
 import scala.language.postfixOps
 
-object Lagrange extends App {
+object WeightedSetCover extends App {
   // val src = Source.fromFile("src/assignment2/data/instanceA.txt")
   val src = Source.fromFile(args(0))
   val line = src.getLines().take(1).reduceLeft(_ + " " + _)
@@ -12,13 +12,13 @@ object Lagrange extends App {
   var m = array(1).toInt // Number of sets.
   var items = Array.ofDim[Int](m, n) // Array containing binary variables to determine whether set i contains item j.
   var numberItems = new Array[Int](n)
-  var c = new Array[Int](m) // Cost of every set.
+  var w = new Array[Int](m) // Cost of every set.
   var elementsIn = new Array[List[Int]](m) // elementsIn is an array containing, for every set, the elements it contains.
 
   var i = 0
   for (line <- src.getLines) {
     val arr = line.split("[ ,\t]").toList
-    c(i) = arr(0).toInt
+    w(i) = arr(0).toInt
     elementsIn(i) = List.empty
     for (j <- 2 until arr.length) {
       val elem: Int = arr(j).toInt - 1
@@ -47,11 +47,11 @@ object Lagrange extends App {
   val maxiter: Int = 5_000_000 // Threshold for iteration.
   val delta: Double = 1e-4 // Threshold for gap.
 
-  var y: Array[Int] = new Array[Int](m) // Decision variables.
+  var x: Array[Int] = new Array[Int](m) // Decision variables.
   var lb: Double = 0.0 // Lower bound on the objective.
 
-  var bestFeasibleY: Array[Int] = Array.fill[Int](m)(1)
-  var bestCost: Int = c.sum // Best-known lower bound objective; we initialize it to the sum of all elements.
+  var bestFeasibleX: Array[Int] = Array.fill[Int](m)(1)
+  var bestCost: Int = w.sum // Best-known lower bound objective; we initialize it to the sum of all elements.
 
   /**
    * Compute a lower bound for the problem.
@@ -66,33 +66,33 @@ object Lagrange extends App {
       // Update the lambdas using the update rule.
       for (j <- lambda.indices) {
         var mul: Int = 1
-        for (i <- setsContaining(j) if y(i) == 1) mul -= 1 // Find the sets in which element j appears.
+        for (i <- setsContaining(j) if x(i) == 1) mul -= 1 // Find the sets in which element j appears.
         lambda(j) = Math.max(0, lambda(j) + mu * mul)
       }
 
       val coeff: Array[Double] = new Array[Double](m) // Coefficients of the decision variables.
-      y = new Array[Int](m)
+      x = new Array[Int](m)
       for (i <- coeff.indices) {
-        coeff(i) = c(i)
+        coeff(i) = w(i)
         for (j <- elementsIn(i)) coeff(i) -= lambda(j) // Update coefficients.
-        if (coeff(i) < 0.0) y(i) = 1 // Decide which sets to take.
+        if (coeff(i) < 0.0) x(i) = 1 // Decide which sets to take.
       }
 
       lb = 0.0
-      for (i <- y.indices if y(i) == 1) lb += c(i)
+      for (i <- x.indices if x(i) == 1) lb += w(i)
       for (j <- lambda.indices) {
         var mul: Int = 1
-        for (i <- setsContaining(j) if y(i) == 1) mul -= 1 // Find the sets in which element j appears.
+        for (i <- setsContaining(j) if x(i) == 1) mul -= 1 // Find the sets in which element j appears.
         lb += lambda(j) * mul
       }
       lb = Math.ceil(lb)
 
       // Update best-known feasible solution if needed.
       if (isFeasible()) {
-        val yCost: Int = cost()
-        if (yCost < bestCost) {
-          bestCost = yCost
-          bestFeasibleY = copy(y) // Copy is needed because y can still change.
+        val xCost: Int = cost()
+        if (xCost < bestCost) {
+          bestCost = xCost
+          bestFeasibleX = copy(x) // Copy is needed because x can still change.
         }
       }
     }
@@ -104,8 +104,8 @@ object Lagrange extends App {
   // otherwise we have a lb and a feasible solution.
   println(if (bestCost - lb < delta) "optimum" else "lower bound with solution")
   println(format(lambda))
-  println(format(y))
-  println(format(bestFeasibleY))
+  println(format(x))
+  println(format(bestFeasibleX))
 
   /**
    * Wrapper for array copy.
@@ -120,11 +120,11 @@ object Lagrange extends App {
 
   /**
    * Cost of a given solution.
-   * @return the cost of y.
+   * @return the cost of x.
    */
   def cost(): Int = {
     var co: Int = 0
-    for (i <- y.indices if y(i) == 1) co += c(i)
+    for (i <- x.indices if x(i) == 1) co += w(i)
     co
   }
 
@@ -135,7 +135,7 @@ object Lagrange extends App {
   def isFeasible(): Boolean = {
     // See which elements are in the solution.
     val elemsTaken: Array[Int] = new Array[Int](n) // Initialize array.
-    for (i <- y.indices if y(i) == 1; j <- elementsIn(i)) elemsTaken(j) = 1 // Set values to 1 when element is taken.
+    for (i <- x.indices if x(i) == 1; j <- elementsIn(i)) elemsTaken(j) = 1 // Set values to 1 when element is taken.
     !elemsTaken.contains(0)
   }
 
